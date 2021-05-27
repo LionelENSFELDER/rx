6039e2feab6f8106e2e1b7899f39891e0a756a54
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-05-27 09:13:42 +0200

    fix(navbar): auto close mobile menu on click fix #6

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React,  {useEffect} from "react";
 
 export default function Navbar(){
+
+  useEffect(() => {
+    autoClose();
+  })
+
+  function autoClose(){
+    const navbar = document.getElementById('mainNavbar');
+    const items = document.querySelectorAll('.nav-item');
+    
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        if(navbar.classList.contains('show')){
+          navbar.classList.remove('show');
+        }
+      })
+    })
+  }
+
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
       <div className="container">
         <a className="navbar-brand" href="/">
           <img className="navbar_logo" src="/logo/logo-rx.png" alt="RX logo" />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div id="navbarTogglerDemo01" className="collapse navbar-collapse">
+        <div id="mainNavbar" className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#prestations">Prestations</a>
